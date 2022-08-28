5baf41c233e3290b4560b8959b1a766f64d82519
Mateus Salvi <mateusalvi@gmail.com>
2022-08-28 20:04:44 -0300

    Sword mouse tracking
    +Sword tracking with mouse
    +Sword visuals
    -Old 3D sword
    -Unused assets

--- Assets/Scripts/swordController.cs
+++ Assets/Scripts/swordController.cs
@@ -8,16 +8,72 @@ public class swordController : MonoBehaviour
     [SerializeField] float distanceFromCamera;
     private Camera cam;
     Vector3 mousePos = new Vector3();
+    public GameObject bladeTrailPrefab;
+    GameObject currentBladeTrail;
+    Vector3 previousPosition;
+    public float minCuttingVelocity = .001f;
+    SphereCollider sphereCollider;
+	private float velocity;
 
     void Start()
     {
+        currentBladeTrail = null;
+        sphereCollider = GetComponent<SphereCollider>();
         cam = Camera.main;
+        mousePos = Input.mousePosition;
+        previousPosition = mousePos;
+    }
+
+    private void FixedUpdate() {
+        UpdateCut();
     }
 
     void Update()
     {
-        mousePos = Input.mousePosition;
-        transform.position = cam.ScreenToWorldPoint(new Vector3 (mousePos.x, mousePos.y, distanceFromCamera));
-        
+
     }
+
+    void UpdateCut ()
+	{
+        mousePos = Input.mousePosition;
+
+        Vector3 newPos = cam.ScreenToWorldPoint(new Vector3 (mousePos.x, mousePos.y, distanceFromCamera));
+        transform.position = newPos;
+
+        velocity = (mousePos - previousPosition).magnitude * Time.deltaTime;
+
+		if (velocity > minCuttingVelocity)
+		{
+            StartCutting();
+		} else
+		{
+            StopCutting();
+		}
+
+		previousPosition = mousePos;
+	}
+
+    void StartCutting ()
+	{
+        sphereCollider.enabled = true;
+        if (currentBladeTrail == null)
+        {
+            currentBladeTrail = Instantiate(bladeTrailPrefab, transform);
+        }
+        else
+        {
+            currentBladeTrail.transform.position = transform.position;
+        }
+	}
+
+	void StopCutting ()
+	{
+		sphereCollider.enabled = false;
+		if(currentBladeTrail != null)
+        {
+            currentBladeTrail.transform.SetParent(null);
+		    Destroy(currentBladeTrail, 2f);
+            currentBladeTrail = null;
+        }
+	}
 }
