9add33ab7bc82e07a8391affcd375c560b2f9715
Mateus Salvi <mateusalvi@gmail.com>
2022-08-29 21:58:25 -0300

    -GlovePie

--- Assets/uiController.cs
+++ Assets/uiController.cs
@@ -4,10 +4,15 @@ using UnityEngine;
 
 public class uiController : MonoBehaviour
 {
+    public int health;
+
+
+    public GameObject[] hearts;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -15,4 +20,7 @@ public class uiController : MonoBehaviour
     {
         
     }
+
+
+
 }
